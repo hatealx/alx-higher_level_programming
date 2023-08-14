4f2c0d67cb29150adc1fa1908fb7f6a34d004ec5
Abel <aatsou12@gmail.com>
2023-08-14 10:15:47 -0700

    done

--- 0x03-python-data_structures/6-print_matrix_integer.py
+++ 0x03-python-data_structures/6-print_matrix_integer.py
@@ -1,6 +1,12 @@
 #!/usr/bin/python3
 def print_matrix_integer(matrix=[[]]):
     for li in matrix:
+        i = 0
         for el in li:
-            print("{}".format(el), end=" ")
+            if (i < 2):
+                print("{}".format(el), end=" ")
+            else:
+                print("{}".format(el), end="")
+            i += 1 
+
         print("")
