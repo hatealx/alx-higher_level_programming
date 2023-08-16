a37d144ebb5543cc7fa48482139e34e351caaec2
Abel <aatsou12@gmail.com>
2023-08-16 10:05:58 -0700

    done

--- 0x04-python-more_data_structures/7-update_dictionary.py
+++ 0x04-python-more_data_structures/7-update_dictionary.py
@@ -1,8 +1,4 @@
 #!/usr/bin/python3
 def update_dictionary(a_dictionary, key, value):
     a_dictionary[key] = value
-
-    for keys in a_dictionary:
-        val = f" {a_dictionary[keys]}"
-        a_dictionary[key] = val
     return a_dictionary
