50362d59591d3df1a7ce99ffebefa868d2389716
Abel <aatsou12@gmail.com>
2023-08-16 10:04:05 -0700

    done

--- 0x04-python-more_data_structures/6-print_sorted_dictionary.py
+++ 0x04-python-more_data_structures/6-print_sorted_dictionary.py
@@ -3,4 +3,4 @@ def print_sorted_dictionary(a_dictionary):
     keys = [k for k in a_dictionary]
     keys = sorted(keys)
     for key in keys:
-        print(f"{key}:{a_dictionary[key]}")
+        print(f"{key}: {a_dictionary[key]}")
--- 0x04-python-more_data_structures/7-update_dictionary.py
+++ 0x04-python-more_data_structures/7-update_dictionary.py
@@ -1,4 +1,8 @@
 #!/usr/bin/python3
 def update_dictionary(a_dictionary, key, value):
-    a_dictionary[key] = f" {value}
+    a_dictionary[key] = value
+
+    for keys in a_dictionary:
+        val = f" {a_dictionary[keys]}"
+        a_dictionary[key] = val
     return a_dictionary
