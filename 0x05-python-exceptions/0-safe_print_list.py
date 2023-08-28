5696b059b40ee50774588ad2da57f8c831aa6362
Abel <aatsou12@gmail.com>
2023-08-28 09:30:54 -0700

    done

---
+++ 0x05-python-exceptions/0-safe_print_list.py
@@ -0,0 +1,12 @@
+#!/usr/bin/python3
+def safe_print_list(my_list=[], x=0):
+    n = 0
+    for i in range(0, x):
+        try:
+            a = my_list[i]
+            print(f"{a}", end="")
+            n += 1
+        except:
+            break
+    print("\n", end="")
+    return (n)
