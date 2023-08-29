ca9471433532cd96954dde3126b367b0420fbadc
Abel <aatsou12@gmail.com>
2023-08-29 07:28:39 -0700

    doen

--- 0x06-python-classes/3-square.py
+++ 0x06-python-classes/3-square.py
@@ -18,6 +18,7 @@ class Square:
                 self.__size = size
         else:
             raise TypeError('size must be an integer')
+
     def area(self):
         """
         finds area of square
