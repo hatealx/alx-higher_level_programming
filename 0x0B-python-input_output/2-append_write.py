900ab3d77d0c05a2d924e9b63917f32ec605a3e3
Abel <aatsou12@gmail.com>
2023-09-19 06:33:41 -0700

    doe

---
+++ 0x0B-python-input_output/2-append_write.py
@@ -0,0 +1,9 @@
+#!/usr/bin/pyhton3
+"""file does not created?"""
+
+
+def append_write(filename="", text=""):
+    """appending to the end"""
+    with open(filename, "a+", encoding="utf8") as file:
+        file.write(text)
+        return len(text)
