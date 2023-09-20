74fbf45e84c7c04b6f9bd7fe90cc604c7c6231ee
Abel <aatsou12@gmail.com>
2023-09-20 05:19:03 -0700

    doen

--- 0x0B-python-input_output/9-student.py
+++ 0x0B-python-input_output/9-student.py
@@ -11,7 +11,6 @@ class Student:
         self.first_name = first_name
         self.last_name = last_name
         self.age = age
-
     def to_json(self):
         """returns a dictionary representation of a Student instance"""
         return self.__dict__
