df36695d5e424e7e98cccfd2f51854a80d1c440a
Abel <aatsou12@gmail.com>
2023-09-21 08:51:17 -0700

    doen

--- 0x0C-python-almost_a_circle/models/rectangle.py
+++ 0x0C-python-almost_a_circle/models/rectangle.py
@@ -55,3 +55,23 @@ class Rectangle(Base):
     def x(self):
         """Set/get the x coordinate of the Rectangle."""
         return self.__x
+    @x.setter
+    def x(self, value):
+        if type(value) != int:
+            raise TypeError("x must be an integer")
+        if value < 0:
+            raise ValueError("x must be >= 0")
+        self.__x = value
+
+    @property
+    def y(self):
+        """Set/get the y coordinate of the Rectangle."""
+        return self.__y
+
+    @y.setter
+    def y(self, value):
+        if type(value) != int:
+            raise TypeError("y must be an integer")
+        if value < 0:
+            raise ValueError("y must be >= 0")
+        self.__y = value
