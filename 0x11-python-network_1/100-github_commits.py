02db081cb6a8767add01330b0ed4ac9827cf0b02
Abel <aatsou12@gmail.com>
2023-12-07 16:12:35 +0000

    done

--- 0x11-python-network_1/100-github_commits.py
+++ 0x11-python-network_1/100-github_commits.py
@@ -1,4 +1,5 @@
-ost recent commits on a given GitHub repository.
+#!/usr/bin/python3
+"""Lists the 10 most recent commits on a given GitHub repository.
 Usage: ./100-github_commits.py <repository name> <repository owner>
 """
 import sys
